95244761c5a968c12a7a00014fe89074551b79ce
khatiwadadarshan <142293704+khatiwadadarshan@users.noreply.github.com>
2023-08-16 18:31:54 -1200

    Add files via upload
    upload

---
+++ class Animal{.java
@@ -0,0 +1,17 @@
+class Animal{
+    void eat(){
+    system.out.println("Änimal is eating");
+    }
+}
+class Dog inherits Animal{
+    void name(){
+        system.out.println("The name is "+name);
+    }
+}
+public class Main {
+    public static void main(String[] args){
+        Dog labrador = new Dog0(){}
+        labrador.name = "Rohu"
+    }
+    
+}
